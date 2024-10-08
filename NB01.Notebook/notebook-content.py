f1873b38c9aed28c3b6b716560274b63575a64db
ziashazcorp <zia@shazcorp.onmicrosoft.com>
2024-10-08 14:52:50 +0530

    Committing 3 items from workspace 0983a460-da38-48e9-b18b-91eaf612f448

---
+++ NB01.Notebook/notebook-content.py
@@ -0,0 +1,30 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "e80dd930-da70-49f3-8ab3-cc94968a6f75",
+# META       "default_lakehouse_name": "LH01",
+# META       "default_lakehouse_workspace_id": "0983a460-da38-48e9-b18b-91eaf612f448",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "e80dd930-da70-49f3-8ab3-cc94968a6f75"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+from numpy import *
+from pandas import *
+from matplotlib import*
+
+# CELL ********************
+
